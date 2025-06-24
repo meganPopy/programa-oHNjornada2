e5195639dd6345a43d9959e53190c51230cc78cd
Tânia <106488757+meganPopy@users.noreply.github.com>
2025-06-24 19:33:09 -0300

    a

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function inserirNome(){
  elemento.textContent = nomeUsuario;
 }
 inserirNome();
+
+function inserirNome() {
+    let nomeUsuario = prompt("Qual o seu nome?");
+    let elemento = document.querySelector("#nome-usuário");
+    elemento.textContent = nomeUsuario;
+    elemento.style.color = corAleatoria(); // Define a cor inicial ao exibir o nome
